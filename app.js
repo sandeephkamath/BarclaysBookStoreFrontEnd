9a44bd0c0dc1315d8f48e325e617547fa25ccc09
Sandeep Kamath <XPlode123>
2021-01-24 18:16:00 +0530

    Production changes.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+let app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || '8080';
+app.set('port', port);
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
