3aabe7fd18cf420814b584fc4464db02da161d61
Sandeep Kamath <XPlode123>
2021-01-25 20:54:26 +0530

    Increased timeout

--- src/apiclient/ApiClient.ts
+++ src/apiclient/ApiClient.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const ApiClient = axios.create({
     baseURL: 'https://barclays-book-store-backend.herokuapp.com/',
-    timeout: 5000,
+    timeout: 50000,
 });
